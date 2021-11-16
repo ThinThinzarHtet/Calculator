49182fc8e5f90b16d27804e378c422254375281a
ThinThinzarHtet <thinthinzarhtet19@gmail.com>
2021-11-16 16:22:27 +0630

    create calculator button

--- script.js
+++ script.js
@@ -1,12 +1,35 @@
-const toggle1 = document.getElementById('toggle');
-toggle1.onclick = function(){
-    if (!toggle1.classList.contains('active')) {
-        toggle1.classList.add('active');
-    } else if(toggle1.classList.contains('active') && !toggle1.classList.contains('active2')) {
-        toggle1.classList.add('active2');
-    } else if (toggle1.classList.contains('active') && toggle1.classList.contains('active2')) {
-        toggle1.classList.remove('active')
-        toggle1.classList.remove('active2')
+const body = document.querySelector('body');
+
+const toggle = document.getElementById('toggle');
+
+const textcolor = document.querySelector('.header');
+
+const output = document.querySelector('.output');
+
+// change theme when click toggle 
+toggle.onclick = function(){
+    if (!toggle.classList.contains('active')) {
+        toggle.classList.add('active');
+        body.classList.add('active');
+        textcolor.classList.add('active');
+        output.classList.add('active');
+        
+    } else if(toggle.classList.contains('active') && !toggle.classList.contains('active2')) {
+        toggle.classList.add('active2');
+        body.classList.add('active2');
+        textcolor.classList.add('active2');
+        output.classList.add('active2');
+       
+    } else if (toggle.classList.contains('active') && toggle.classList.contains('active2')) {
+        toggle.classList.remove('active');
+        toggle.classList.remove('active2');
+        body.classList.remove('active');
+        body.classList.remove('active2');
+        textcolor.classList.remove('active');
+        textcolor.classList.remove('active2');
+        output.classList.remove('active');
+        output.classList.remove('active2');
+
     }
     
 
